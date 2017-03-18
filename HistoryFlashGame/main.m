27718896b3c6282ad6c8d943f7e7f5376eff73f2
Kemuel Clyde Belderol <Belderol789@kemuels-mac.local>
2017-03-17 23:26:49 -0700

    Initial Commit

---
+++ HistoryFlashGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HistoryFlashGame
+//
+//  Created by Kemuel Clyde Belderol on 17/03/2017.
+//  Copyright © 2017 Burst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
